4791a0fda2e660b7f9b77f5d2a6a5ffbb286ba77
Alex M <48068999+aleexmaftei@users.noreply.github.com>
2022-01-12 17:06:04 +0200

    Read from file

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,19 +1,24 @@
 #include<cstdio>
 #include<cstdlib>
+#include<string>
+#include <iostream>
+#include<fstream>
+#include <vector>
+
+using namespace std;
 
 typedef struct proces {
-    char nume[5];
+    string nume;
     int timpOcupare;  /// durata procesului
     int timpInceput;  /// cand incepe procesul
     int prioritate;
     int timpAsteptare, timpProcesare;
     int verificat;
-}
-        procese;
+} proces;
 
 // TODO: quicksort
-void bubble_sort(procese temp[], int n) {
-    procese t;
+void bubble_sort(proces temp[], int n) {
+    proces t;
     /// procesele sunt sortate crescator dupa timpul de inceput
     /// primul din vector va fi primul care ajunge la cpu
 
@@ -30,14 +35,14 @@ void bubble_sort(procese temp[], int n) {
     }
 }
 
-int accept(procese P[]) {
+int accept(proces P[]) {
     int i, n;
-    printf("\n Introdu numar procese : ");
+    printf("\n Introdu numar proces : ");
     scanf("%d", &n);
     for (i = 0; i < n; i++) {
         printf("\n PROCESUL [%d]", i + 1);
         printf(" Introdu nume proces : ");
-        scanf("%s", P[i].nume);
+        scanf("%s", P[i].nume.c_str());
         printf(" Introdu timp ocupare : ");
         scanf("%d", &P[i].timpOcupare);
         printf(" Introdu timpul de inceput : ");
@@ -47,13 +52,40 @@ int accept(procese P[]) {
     }
     printf("\n PROC.\tB.T.\tA.T.\tPRIORITY");
     for (i = 0; i < n; i++)
-        printf("\n %s\t%d\t%d\t%d", P[i].nume, P[i].timpOcupare, P[i].timpInceput, P[i].prioritate);
+        printf("\n %s\t%d\t%d\t%d", P[i].nume.c_str(), P[i].timpOcupare, P[i].timpInceput, P[i].prioritate);
     return n;
 }
 
+pair<vector<proces>, int> fileRead(string path) {
+    string nume, timpOcupare, timpInceput, prioritate, n;
+
+    pair<vector<proces>, int> values;
+    ifstream fin(path);
+    getline(fin, n, '\n');
+    proces arr[stoi(n)];
+
+    while (getline(fin, nume, ',') &&
+           getline(fin, timpOcupare, ',') &&
+           getline(fin, timpInceput, ',') &&
+           getline(fin, prioritate, '\n')) {
+        proces p;
+        p.nume = nume;
+        p.timpOcupare = stoi(timpOcupare);
+        p.timpInceput = stoi(timpInceput);
+        p.prioritate = stoi(prioritate);
+        p.verificat = 0;
+        values.first.push_back(p);
+    }
+
+    values.second = stoi(n);
+
+    fin.close();
+    return values;
+}
+
 /// FCFS Algorithm - FIFO (primul venit primul servit), cel mai slab ca timp
-void FCFS(procese P[], int n) {
-    procese temp[10];
+void FCFS(vector<proces> P, int n) {
+    proces temp[n];
     int sumaTimpAsteptare = 0, sumaTimpProcesare = 0;
     int x = 0;
     float timpMediuAsteptare = 0.0, timpMediuProcesare = 0.0;
@@ -66,7 +98,7 @@ void FCFS(procese P[], int n) {
 
     printf("\n\n PROC.\tB.T.\tA.T.");
     for (i = 0; i < n; i++)
-        printf("\n %s\t%d\t%d", temp[i].nume, temp[i].timpOcupare, temp[i].timpInceput);
+        printf("\n %s\t%d\t%d", temp[i].nume.c_str(), temp[i].timpOcupare, temp[i].timpInceput);
 
     sumaTimpAsteptare = temp[0].timpAsteptare = 0;
     sumaTimpProcesare = temp[0].timpProcesare = temp[0].timpOcupare - temp[0].timpInceput;
@@ -83,12 +115,12 @@ void FCFS(procese P[], int n) {
 
     printf("\n\n PROC.\tB.T.\tA.T.\tW.T\tT.A.T");
     for (i = 0; i < n; i++)
-        printf("\n %s\t%d\t%d\t%d\t%d", temp[i].nume, temp[i].timpOcupare, temp[i].timpInceput, temp[i].timpAsteptare,
+        printf("\n %s\t%d\t%d\t%d\t%d", temp[i].nume.c_str(), temp[i].timpOcupare, temp[i].timpInceput, temp[i].timpAsteptare,
                temp[i].timpProcesare);
 
     printf("\n\n GANTT CHART\n ");
     for (i = 0; i < n; i++)
-        printf("   %s   ", temp[i].nume);
+        printf("   %s   ", temp[i].nume.c_str());
     printf("\n ");
 
     printf("0\t");
@@ -101,9 +133,9 @@ void FCFS(procese P[], int n) {
 }
 
 // SJF Non Pre-emptive
-void SJF_NP(procese P[], int n) {
-    procese temp[10];
-    procese t;
+void SJF_NP(vector<proces> P, int n) {
+    proces temp[n];
+    proces t;
     int sumaTimpAsteptare = 0, sumaTimpProcesare = 0;
     int x = 0;
     float timpMediuAsteptare = 0.0, timpMediuProcesare = 0.0;
@@ -125,7 +157,7 @@ void SJF_NP(procese P[], int n) {
 
     printf("\n\n PROC.\tB.T.\tA.T.");
     for (i = 0; i < n; i++)
-        printf("\n %s\t%d\t%d", temp[i].nume, temp[i].timpOcupare, temp[i].timpInceput);
+        printf("\n %s\t%d\t%d", temp[i].nume.c_str(), temp[i].timpOcupare, temp[i].timpInceput);
 
     sumaTimpAsteptare = temp[0].timpAsteptare = 0;
     sumaTimpProcesare = temp[0].timpProcesare = temp[0].timpOcupare - temp[0].timpInceput;
@@ -143,12 +175,12 @@ void SJF_NP(procese P[], int n) {
 
     printf("\n\n PROC.\tB.T.\tA.T.\tW.T\tT.A.T");
     for (i = 0; i < n; i++)
-        printf("\n %s\t%d\t%d\t%d\t%d", temp[i].nume, temp[i].timpOcupare, temp[i].timpInceput, temp[i].timpAsteptare,
+        printf("\n %s\t%d\t%d\t%d\t%d", temp[i].nume.c_str(), temp[i].timpOcupare, temp[i].timpInceput, temp[i].timpAsteptare,
                temp[i].timpProcesare);
 
     printf("\n\n GANTT CHART\n ");
     for (i = 0; i < n; i++)
-        printf("   %s   ", temp[i].nume);
+        printf("   %s   ", temp[i].nume.c_str());
     printf("\n ");
 
     printf("0\t");
@@ -162,9 +194,9 @@ void SJF_NP(procese P[], int n) {
 
 /// Priority Non Pre-emptive - primul cu prioritatea cea mai mare este luat primul si executat
 /// fiecare in ordinea data de prioritate se asteapta pana se termina si dupa trece la urmatorul
-void PRT_NP(procese P[], int n) {
-    procese temp[10];
-    procese t;
+void PRT_NP(vector<proces> P, int n) {
+    proces temp[n];
+    proces t;
     int sumaTimpAsteptare = 0, sumaTimpProcesare = 0;
     float timpMediuAsteptare = 0.0, timpMediuProcesare = 0.0;
     int i, j;
@@ -186,7 +218,7 @@ void PRT_NP(procese P[], int n) {
 
     printf("\n\n PROC.\tTimp Ocupare\tTimp Inceput");
     for (i = 0; i < n; i++)
-        printf("\n %s\t%d\t\t%d", temp[i].nume, temp[i].timpOcupare, temp[i].timpInceput);
+        printf("\n %s\t%d\t\t%d", temp[i].nume.c_str(), temp[i].timpOcupare, temp[i].timpInceput);
 
     sumaTimpAsteptare = temp[0].timpAsteptare = 0;
     sumaTimpProcesare = temp[0].timpProcesare = temp[0].timpOcupare - temp[0].timpInceput;
@@ -204,12 +236,12 @@ void PRT_NP(procese P[], int n) {
 
     printf("\n\n PROC.\tTimp Ocupare\tTimp Inceput\tTimp Asteptare\tTimp Procesare");
     for (i = 0; i < n; i++)
-        printf("\n %s\t%d\t\t%d\t\t%d\t\t%d", temp[i].nume, temp[i].timpOcupare, temp[i].timpInceput,
+        printf("\n %s\t%d\t\t%d\t\t%d\t\t%d", temp[i].nume.c_str(), temp[i].timpOcupare, temp[i].timpInceput,
                temp[i].timpAsteptare, temp[i].timpProcesare);
 
     printf("\n\n GANTT CHART\n ");
     for (i = 0; i < n; i++)
-        printf("   %s   ", temp[i].nume);
+        printf("   %s   ", temp[i].nume.c_str());
     printf("\n ");
 
     printf("0\t");
@@ -222,11 +254,11 @@ void PRT_NP(procese P[], int n) {
 }
 
 /// Round Robin Scheduling - preentiv - poate fi intrerupt si sa continue mai tarziu in cazul in care a ajuns la quantumul introdus
-void RR(procese P[], int n) {
+void RR(vector<proces> P, int n) {
     int nrProceseVerif = 0, t, timpCurent = 0, k, i, Q = 0;
     int sumaTimpAsteptare = 0, sumaTimpProcesare = 0;
     float timpMediuAsteptare = 0.0, timpMediuProcesare = 0.0;
-    procese temp1[10], temp2[10];
+    proces temp1[n], temp2[n];
 
     for (i = 0; i < n; i++)
         temp1[i] = P[i];
@@ -245,7 +277,7 @@ void RR(procese P[], int n) {
             k = 0;
 
         if (temp1[k].timpOcupare > 0)
-            printf("(%s  %d)", temp1[k].nume, timpCurent);
+            printf("(%s  %d)", temp1[k].nume.c_str(), timpCurent);
         // printf("(  %d  %s)", timpCurent, temp1[k].nume);
         t = 0;
 
@@ -279,11 +311,11 @@ void RR(procese P[], int n) {
 }
 
 // Shortest Job First - Pre-emptive
-void SJF_P(procese P[], int n) {
-    int i, t_total = 0, timpCurent, b[10], min_timpInceput, j, x, min_timpOcupare;
+void SJF_P(vector<proces> P, int n) {
+    int i, t_total = 0, timpCurent, b[n], j, x, min_timpOcupare;
     int sumaTimpAsteptare = 0, sumaTimpProcesare = 0;
     float timpMediuAsteptare = 0.0, timpMediuProcesare = 0.0;
-    procese temp[10], t;
+    proces temp[n], t;
 
     for (i = 0; i < n; i++) {
         temp[i] = P[i];
@@ -296,13 +328,13 @@ void SJF_P(procese P[], int n) {
         b[i] = temp[i].timpOcupare;
 
     i = j = 0;
-    printf("\n GANTT CHART\n\n %d %s", i, temp[i].nume);
+    printf("\n GANTT CHART\n\n %d %s", i, temp[i].nume.c_str());
     for (timpCurent = 0; timpCurent < t_total; timpCurent++) {
         if (b[i] > 0 && temp[i].timpInceput <= timpCurent)
             b[i]--;
 
         if (i != j)
-            printf(" %d %s", timpCurent, temp[i].nume);
+            printf(" %d %s", timpCurent, temp[i].nume.c_str());
 
         if (b[i] <= 0 && temp[i].verificat != 1) {
             temp[i].verificat = 1;
@@ -312,7 +344,7 @@ void SJF_P(procese P[], int n) {
             sumaTimpProcesare += temp[i].timpProcesare;
         }
         j = i;
-        min_timpOcupare = 999;
+        min_timpOcupare = 9999;
         for (x = 0; x < n; x++) {
             if (temp[x].timpInceput <= (timpCurent + 1) && temp[x].verificat != 1) {
                 if (min_timpOcupare != b[x] && min_timpOcupare > b[x]) {
@@ -329,11 +361,11 @@ void SJF_P(procese P[], int n) {
            timpMediuProcesare);
 }
 
-void PRT_P(procese P[], int n) {
-    int i, t_total = 0, timpCurent, b[10], j, x, min_pr;
+void PRT_P(vector<proces> P, int n) {
+    int i, t_total = 0, timpCurent, b[n], j, x, min_pr;
     int sumaTimpAsteptare = 0, sumaTimpProcesare = 0;
     float timpMediuAsteptare = 0.0, timpMediuProcesare = 0.0;
-    procese temp[10];
+    proces temp[n];
 
     for (i = 0; i < n; i++) {
         temp[i] = P[i];
@@ -346,13 +378,13 @@ void PRT_P(procese P[], int n) {
         b[i] = temp[i].timpOcupare;
 
     i = j = 0;
-    printf("\n GANTT CHART\n\n %d %s", i, temp[i].nume);
+    printf("\n GANTT CHART\n\n %d %s", i, temp[i].nume.c_str());
     for (timpCurent = 0; timpCurent < t_total; timpCurent++) {
         if (b[i] > 0 && temp[i].timpInceput <= timpCurent)
             b[i]--;
 
         if (i != j)
-            printf(" %d %s", timpCurent, temp[i].nume);
+            printf(" %d %s", timpCurent, temp[i].nume.c_str());
 
         if (b[i] <= 0 && temp[i].verificat != 1) {
             temp[i].verificat = 1;
@@ -362,7 +394,7 @@ void PRT_P(procese P[], int n) {
             sumaTimpProcesare += temp[i].timpProcesare;
         }
         j = i;
-        min_pr = 999;
+        min_pr = 9999;
         for (x = 0; x < n; x++) {
             if (temp[x].timpInceput <= (timpCurent + 1) && temp[x].verificat != 1) {
                 if (min_pr != temp[x].prioritate && min_pr > temp[x].prioritate) {
@@ -380,11 +412,15 @@ void PRT_P(procese P[], int n) {
 }
 
 int main() {
-    procese P[10];
     int ch, n;
+    string path = "../../data/data_50.txt";
+    pair<vector<proces>, int> values;
+    values = fileRead(path);
+
+    n = values.second;
+    vector<proces> P = values.first;
     do {
         printf("\n Optiuni:");
-        printf("\n 0. Introdu informatiile procesului.");
         printf("\n 1. FCFS");
         printf("\n 2. SJF (Pre-emptive)");
         printf("\n 3. SJF (Non Pre-emptive)");
@@ -394,9 +430,6 @@ int main() {
         printf("\n 7. Exit\n Optiunea ta : ");
         scanf("%d", &ch);
         switch (ch) {
-            case 0:
-                n = accept(P);
-                break;
             case 1:
                 FCFS(P, n);
                 break;
@@ -415,7 +448,7 @@ int main() {
             case 6:
                 RR(P, n);
                 break;
-            case 7:
+            default:
                 exit(0);
         }
     } while (ch != 7);
