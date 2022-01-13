373a3474bba51ab05ab6863ab789f55b709f1704
Alex Benchea <alexbenchea99@yahoo.com>
2022-01-13 15:49:45 -0500

    modify: process model

--- python/model/Process.py
+++ python/model/Process.py
@@ -6,6 +6,8 @@ class Process:
     self.priority = priority
     self.arrival_time = arrival_time
     self.completion_time = 0
+    self.turn_around_time = 0
+    self.waiting_time = 0
 
   def __str__(self):
     return f'{self.name} - {self.duration} - {self.priority} - {self.completion_time} - {self.arrival_time}'
